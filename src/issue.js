cbfcf18e0ddb29e09fad6260961573e70ae5f4c6
Ziga Vukcevic <ziga@be-codified.com>
2019-12-29 14:29:50 +0100

    Update readme, add small fixes

--- src/issue.js
+++ src/issue.js
@@ -12,49 +12,52 @@ const { log } = console;
 
 const issue = (argv) => {
   if (isConfigSet()) {
-    const spinner = ora('Fetching...').start();
-    // log(chalk.yellow(`\nGetting issue with id ${argv.id}`));
+    if (!argv.id) {
+      log(chalk.red('You\'re missing id of issue. Sample: `happy-jira issue --id=JIRA-ISSUE-ID`\n'));
+    } else {
+      const spinner = ora('Fetching...').start();
 
-    fetch(`https://${subdomain}/rest/api/3/issue/${argv.id}`, {
-      method: 'get',
-      headers: {
-        'Authorization': `Basic ${encode(`${username}:${token}`)}`, // eslint-disable-line quote-props
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((res) => {
-        spinner.stop();
-
-        if (res.status === 401) {
-          throw chalk.red('Authentication failed, check your config.');
-        } else if (res.status === 404) {
-          throw chalk.red(`Issue ${argv.id} not found.\n`);
-        } else if (res.status === 200) {
-          return res.json();
-        } else {
-          return false;
-        }
+      fetch(`https://${subdomain}/rest/api/3/issue/${argv.id}`, {
+        method: 'get',
+        headers: {
+          'Authorization': `Basic ${encode(`${username}:${token}`)}`, // eslint-disable-line quote-props
+          'Content-Type': 'application/json',
+        },
       })
-      .then((json) => {
-        const { fields } = json;
+        .then((res) => {
+          spinner.stop();
 
-        log('ID:          ', argv.id);
-        log('Project name:', fields.project.name);
-        log('Created:     ', moment(fields.created).format('dddd MM-DD-YYYY'));
-        log('Assignee:    ', fields.assignee.displayName);
-        log('Summary:     ', fields.summary);
-        log('Status:      ', fields.status.name);
-        log('Total time:  ', formatTimeHHmm(fields.timespent));
-      })
-      .catch((error) => {
-        spinner.stop();
+          if (res.status === 401) {
+            throw chalk.red('Authentication failed, check your config.');
+          } else if (res.status === 404) {
+            throw chalk.red(`Issue ${argv.id} not found.\n`);
+          } else if (res.status === 200) {
+            return res.json();
+          } else {
+            return false;
+          }
+        })
+        .then((json) => {
+          const { fields } = json;
 
-        if (error.code === 'ENOTFOUND') {
-          log(chalk.red(`Subdomain ${subdomain} has not been found. Are you connected to Internet?\n`));
-        } else {
-          log(error);
-        }
-      });
+          log('ID:             ', argv.id);
+          log('Project name:   ', fields.project.name);
+          log('Created:        ', moment(fields.created).format('dddd MM-DD-YYYY'));
+          log('Assignee:       ', fields.assignee.displayName);
+          log('Summary:        ', fields.summary);
+          log('Status:         ', fields.status.name);
+          log('Total worklog:  ', formatTimeHHmm(fields.timespent));
+        })
+        .catch((error) => {
+          spinner.stop();
+
+          if (error.code === 'ENOTFOUND') {
+            log(chalk.red(`Subdomain ${subdomain} has not been found. Are you connected to Internet?\n`));
+          } else {
+            log(error);
+          }
+        });
+    }
   }
 };
 
--- src/worklog.js
+++ src/worklog.js
@@ -12,52 +12,56 @@ const { log } = console;
 
 const worklog = (argv) => {
   if (isConfigSet()) {
-    const spinner = ora('Fetching...').start();
+    if (!argv.id) {
+      log(chalk.red('You\'re missing id of issue. Sample: `happy-jira worklog --id=JIRA-ISSUE-ID`\n'));
+    } else {
+      const spinner = ora('Fetching...').start();
 
-    fetch(`https://${subdomain}/rest/api/3/issue/${argv.id}/worklog`, {
-      method: 'get',
-      headers: {
-        'Authorization': `Basic ${encode(`${username}:${token}`)}`, // eslint-disable-line quote-props
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((res) => {
-        spinner.stop();
-
-        if (res.status === 401) {
-          throw chalk.red('Authentication failed, check your config.');
-        } else if (res.status === 404) {
-          throw chalk.red(`Issue ${argv.id} not found.\n`);
-        } else if (res.status === 200) {
-          return res.json();
-        } else {
-          return false;
-        }
+      fetch(`https://${subdomain}/rest/api/3/issue/${argv.id}/worklog`, {
+        method: 'get',
+        headers: {
+          'Authorization': `Basic ${encode(`${username}:${token}`)}`, // eslint-disable-line quote-props
+          'Content-Type': 'application/json',
+        },
       })
-      .then((json) => {
-        let timeTotal = 0;
+        .then((res) => {
+          spinner.stop();
 
-        json.worklogs.forEach((item) => {
-          log(
-            moment(item.created).format('dddd MM-DD-YYYY'),
-            item.author.displayName,
-            formatTimeHHmm(item.timeSpentSeconds),
-          );
+          if (res.status === 401) {
+            throw chalk.red('Authentication failed, check your config.');
+          } else if (res.status === 404) {
+            throw chalk.red(`Issue ${argv.id} not found.\n`);
+          } else if (res.status === 200) {
+            return res.json();
+          } else {
+            return false;
+          }
+        })
+        .then((json) => {
+          let timeTotal = 0;
 
-          timeTotal += item.timeSpentSeconds;
+          json.worklogs.forEach((item) => {
+            log(
+              moment(item.created).format('dddd MM-DD-YYYY'),
+              item.author.displayName,
+              formatTimeHHmm(item.timeSpentSeconds),
+            );
+
+            timeTotal += item.timeSpentSeconds;
+          });
+
+          log('Total time:', formatTimeHHmm(timeTotal));
+        })
+        .catch((error) => {
+          spinner.stop();
+
+          if (error.code === 'ENOTFOUND') {
+            log(chalk.red(`Subdomain ${subdomain} has not been found. Are you connected to Internet?\n`));
+          } else {
+            log(error);
+          }
         });
-
-        log('Total time:', formatTimeHHmm(timeTotal));
-      })
-      .catch((error) => {
-        spinner.stop();
-
-        if (error.code === 'ENOTFOUND') {
-          log(chalk.red(`Subdomain ${subdomain} has not been found. Are you connected to Internet?\n`));
-        } else {
-          log(error);
-        }
-      });
+    }
   }
 };
 
